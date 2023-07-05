dba273c9efb9ba50b823ed58a2ccf1cb9c8436e9
youngchive <2000mily@naver.com>
2023-07-05 18:32:16 +0900

    기본 뼈대 다시 생성 및 커뮤니티, 회원가입, 로그인 생성

---
+++ withChaegi-Project/essays/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Essay(models.Model):
+    essay_title = models.TextField(verbose_name='감상문 제목')
+    book_photo = models.ImageField(verbose_name='사진 첨부', null=True, blank=True)
+    book_title = models.TextField(verbose_name='책 제목')
+    book_author = models.TextField(verbose_name='책 지은이')
+    essay_content = models.TextField(verbose_name='상세 내용')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
+    view_count = models.IntegerField(verbose_name='조회수', default=0)
+    writer = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True)
+
+class Comment(models.Model):
+    comment_content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(verbose_name='작성일')
+    essay = models.ForeignKey(to='Essay', on_delete=models.CASCADE)
+    writer = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, blank=True)
---
+++ withChaegi-Project/withChaegi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WithchaegiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'withChaegi'
