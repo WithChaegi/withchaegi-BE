ac788654c0179009df8742d97d11e261eb587b81
youngchive <2000mily@naver.com>
2023-07-07 16:36:08 +0900

    감상문, 댓글 관리

--- withChaegi-Project/essays/admin.py
+++ withChaegi-Project/essays/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 
 from .models import Essay, Comment
 
-admin.site.register(Essay)
-admin.site.register(Comment)
+class CommentInline(admin.StackedInline):
+    # 가로는 TabularInline
+    model = Comment
+    extra = 3
+    verbose_name = '댓글'
+    verbose_name_plural = '댓글'
+
+@admin.register(Essay)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'essay_title', 'writer', 'book_title', 'book_author', 'essay_content', 'created_at', 'view_count')
+    #search_fields = ('book_title')
+    inlines = [CommentInline]
+
+# admin.site.register(Comment)
