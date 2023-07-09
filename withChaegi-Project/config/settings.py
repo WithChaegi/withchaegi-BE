ec19180c5b02b7770ee4d642804adcf0cf1496f9
youngchive <2000mily@naver.com>
2023-07-10 03:35:25 +0900

    시간설정 변경

--- withChaegi-Project/config/settings.py
+++ withChaegi-Project/config/settings.py
@@ -110,11 +110,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
