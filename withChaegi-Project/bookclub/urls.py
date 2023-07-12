afe995b141977e0b4acfb7a3283400d2042d5374
Yaeran Kim <yaerankim000812@gmail.com>
2023-07-12 21:00:41 +0900

    독서모임 페이지 전환 관련 코드 및 모델 일부 수정

--- withChaegi-Project/bookclub/urls.py
+++ withChaegi-Project/bookclub/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import entire_list, popular_list, club_detail, create_club, apply_club
+from .views import main_page, entire_list, popular_list, club_detail, create_club, apply_club
 
 app_name = 'bookclub'
 
 urlpatterns = [
+    path('main', main_page, name='main_page'),
     path('entirelist', entire_list, name='entirelist'),
     path('popularlist', popular_list, name='popularlist'),
     path('<int:pk>', club_detail, name='club_detail'),
